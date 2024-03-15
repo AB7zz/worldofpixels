a46dd120d331c0e7e701c9baa9a35e2de0ea5243
unknown <abhinavcv007@gmail.com>
2024-03-15 20:55:59 +0530

    added dynamic colors, and pixels

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,249 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './app.css'
+import { motion } from 'framer-motion';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [color, setColor] = React.useState('')
+  const [newRect, setNewRect] = React.useState({ x: 0, y: 0, width: 0, height: 0 })
+
+  React.useEffect(() => {
+    const canvas = document.getElementById('pixelCanvas');
+    const ctx = canvas.getContext('2d');
+
+    // Define canvas properties
+    const canvasWidth = 2500;
+    const canvasHeight = 2500;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    let cameraOffset = { x: window.innerWidth/2, y: window.innerHeight/2 }
+    let isDragging = false;
+    let SCROLL_SENSITIVITY = 0.0005
+    let cameraZoom = 1
+    let MAX_ZOOM = 5
+    let MIN_ZOOM = 0.1    
+
+    const pixelSize = 10;
+
+    const rectWidth = 10;
+    const rectHeight = 10;
+    const spacing = 1;
+
+    function draw(){
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+
+      ctx.translate( -window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y )
+      
+      ctx.scale(cameraZoom, cameraZoom)
+      
+      createRectanglePattern()
+
+      requestAnimationFrame(draw)
+    }
+
+    function drawRectangle(x, y) {
+      ctx.fillStyle = '#fafafa';
+      ctx.fillRect(x, y, rectWidth, rectHeight);
+    }
+
+    let rectangles = []
+
+    function createRectanglePattern() {
+      for (let i = 0; i < canvas.width; i += (rectWidth + spacing)) {
+        for (let j = 0; j < canvas.height; j += (rectHeight + spacing)) {
+          drawRectangle(i, j);
+          const rect = {
+            x: i,
+            y: j,
+            width: 10,
+            height: 10
+          }
+          rectangles.push(rect)
+        }
+      }
+    }
+
+    createRectanglePattern()
+
+    function isMouseInsideRect(x, y, rect) {
+      return x >= rect.x && x <= rect.x + rect.width &&
+            y >= rect.y && y <= rect.y + rect.height;
+    }
+
+    
+
+    let dragStart = { x: 0, y: 0 }
+
+    function onPointerDown(e)
+    {
+        isDragging = true
+        dragStart.x = getEventLocation(e).x/cameraZoom - cameraOffset.x
+        dragStart.y = getEventLocation(e).y/cameraZoom - cameraOffset.y
+    }
+
+    function onPointerUp(e)
+    {
+        isDragging = false
+        initialPinchDistance = null
+        lastZoom = cameraZoom
+    }
+
+    function onPointerMove(e)
+    {
+        if (isDragging)
+        {
+            cameraOffset.x = getEventLocation(e).x/cameraZoom - dragStart.x
+            cameraOffset.y = getEventLocation(e).y/cameraZoom - dragStart.y
+        }
+    }
+
+    function handleTouch(e, singleTouchHandler)
+    {
+        if ( e.touches.length == 1 )
+        {
+            singleTouchHandler(e)
+        }
+        else if (e.type == "touchmove" && e.touches.length == 2)
+        {
+            isDragging = false
+            handlePinch(e)
+        }
+    }
+
+    let initialPinchDistance = null
+    let lastZoom = cameraZoom
+
+    function handlePinch(e)
+    {
+        e.preventDefault()
+        
+        let touch1 = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+        let touch2 = { x: e.touches[1].clientX, y: e.touches[1].clientY }
+        
+        // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
+        let currentDistance = (touch1.x - touch2.x)**2 + (touch1.y - touch2.y)**2
+        
+        if (initialPinchDistance == null)
+        {
+            initialPinchDistance = currentDistance
+        }
+        else
+        {
+            adjustZoom( null, currentDistance/initialPinchDistance )
+        }
+    }
+
+    function adjustZoom(zoomAmount, zoomFactor)
+    {
+      if (!isDragging)
+      {
+        if (zoomAmount)
+        {
+            cameraZoom += zoomAmount
+        }
+        else if (zoomFactor)
+        {
+            console.log(zoomFactor)
+            cameraZoom = zoomFactor*lastZoom
+        }
+        
+        cameraZoom = Math.min( cameraZoom, MAX_ZOOM )
+        cameraZoom = Math.max( cameraZoom, MIN_ZOOM )
+
+        console.log(cameraZoom)
+
+        ctx.scale(cameraZoom, cameraZoom)
+
+      }
+    }
+
+    canvas.addEventListener('click', function(event) {
+      const mouseX = event.offsetX;
+      const mouseY = event.offsetY;
+      rectangles.forEach(rect => {
+        if (isMouseInsideRect(mouseX, mouseY, rect)) {
+          const newRect = {
+            x: rect.x,
+            y: rect.y,
+            width: rect.width,
+            height: rect.height
+          }
+          setNewRect(newRect)
+          
+        }
+      })
+    })
+    // canvas.addEventListener('mousedown', onPointerDown)
+    // canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
+    // canvas.addEventListener('mouseup', onPointerUp)
+    // canvas.addEventListener('touchend',  (e) => handleTouch(e, onPointerUp))
+    // canvas.addEventListener('mousemove', onPointerMove)
+    // canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove))
+    // canvas.addEventListener( 'wheel', (e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY))
+
+    // draw()
+      
+  }, [])
+
+  React.useEffect(() => {
+    const canvas = document.getElementById('pixelCanvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = color;
+    ctx.fillRect(newRect.x, newRect.y, newRect.width, newRect.height);
+  }, [newRect])
+
+  const handleColor = (newcolor) => {
+    setColor(newcolor)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='absolute z-20 left-[30%]'>
+      <div className='bg-white rounded-[100px] px-5 py-3 w-[500px]'>
+        <div className='flex justify-around'>
+          <motion.div
+            onClick={() => handleColor('red')}
+            whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 1 }}
+            className='hover:cursor-pointer bg-red-500 rounded-[50%] px-5 py-5'>
+          </motion.div>
+          <motion.div
+            onClick={() => handleColor('green')}
+            whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 1 }}
+            className='hover:cursor-pointer bg-green-500 rounded-[50%] px-5 py-5'>
+          </motion.div>
+          <motion.div
+            onClick={() => handleColor('blue')}
+            whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 1 }}
+            className='hover:cursor-pointer bg-blue-500 rounded-[50%] px-5 py-5'>
+          </motion.div>
+          <motion.div
+            onClick={() => handleColor('orange')}
+            whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 1 }}
+            className='hover:cursor-pointer bg-orange-500 rounded-[50%] px-5 py-5'>
+          </motion.div>
+          <motion.div
+            onClick={() => handleColor('yellow')}
+            whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 1 }}
+            className='hover:cursor-pointer bg-yellow-500 rounded-[50%] px-5 py-5'>
+          </motion.div>
+          <motion.div
+            onClick={() => handleColor('purple')}
+            whileHover={{ scale: 1.2 }}
+            // whileTap={{ scale: 1 }}
+            className='hover:cursor-pointer bg-purple-500 rounded-[50%] px-5 py-5'>
+          </motion.div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </div>
+      <div id="canvasContainer">
+        <canvas className='' id="pixelCanvas"></canvas>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
